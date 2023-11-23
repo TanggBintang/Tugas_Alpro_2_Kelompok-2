8c0f0bc9a8db7afa89d48b07711faf0b195be4c8
TanggBintang <149054119+TanggBintang@users.noreply.github.com>
2023-11-23 23:22:42 +0700

    Update TugasRobot.py

--- TugasRobot.py
+++ TugasRobot.py
@@ -76,3 +76,25 @@ while at_goal()==False:
         lompat()
     elif not wall_in_front():
         move()
+-----------------------MAZE------------------------
+kondisi():
+    while wall_in_front()==False:
+        move()
+        if wall_in_front()==False and right_is_clear()==True:
+            turn_right()
+            move()
+    if wall_in_front()==True and right_is_clear()==False:
+        turn_left()
+    elif wall_in_front()==True and right_is_clear()==True:
+        turn_right()
+        if is_facing_north():
+            while wall_in_front()==False:
+                move()
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+while at_goal()==False:
+    kondisi()
